dfe2d109f7fa985ac89e2d5f6066538dfe7e1736
Keisuke SASAKI <sa2ki.k32@gmail.com>
2014-09-30 17:12:37 +0900

    cope multiple lines

--- parser.rb
+++ parser.rb
@@ -1,21 +1,22 @@
 open(ARGV[0]) { |f|
   parsing = false
   key = ""
-  body = ""
+  body = Array.new
   f.each { |line|
     line.gsub!(/\n/, '')
     next if line.empty?
     if parsing
       if md = line.match(/(\/@\w+)/)
+        puts '"' +  key + '" = "' + body.join('\n') + '";'
         parsing = false
       else
         unless line.match(/\[\[\[.*\]\]\]/)
-         body = line
-         puts '"' +  key + '" = "' + body + '";'
+         body << line
         end
       end
     else
       if md = line.match(/(@\w+)/)
+        body = Array.new
         key = md[0].gsub("@", "")
         parsing = true
       end
